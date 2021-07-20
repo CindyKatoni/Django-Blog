d03cfa5211d5430edabaf9a2a9e33f62908222e7
CindyKatoni <katmuema@gmail.com>
2021-07-20 16:14:27 +0300

    template inheritance, add bootstrap, add static files

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+ 
+posts = [
+    {
+        'title': 'Blog Post 1',
+        'author': 'Cindy Kat',
+        'content': 'Im a software developer',
+        'date_posted': 'July 20, 2021'
+    },
+    {
+         'title': 'Blog Post 2',
+        'author': 'Lucy Kamene',
+        'content': 'Im a penetration tester',
+        'date_posted': 'July 20, 2021'
+    }
+]
 
 # Create your views here.
 def index(request):
-    title = 'Welcome To The Blog'
-    return render(request, 'blog/home.html', {'title':title})
+    
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-     title = 'About'
-     return render(request, 'blog/about.html', {'title':title})   
+     return render(request, 'blog/about.html', {'title':'About'})   
