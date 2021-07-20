2bd8469b94e3d7fa6876829d00754511e545d10b
CindyKatoni <katmuema@gmail.com>
2021-07-20 20:13:12 +0300

    magic method, registered the model in admin

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
+from .models import Post
  
-posts = [
-    {
-        'title': 'Blog Post 1',
-        'author': 'Cindy Kat',
-        'content': 'Im a software developer',
-        'date_posted': 'July 20, 2021'
-    },
-    {
-         'title': 'Blog Post 2',
-        'author': 'Lucy Kamene',
-        'content': 'Im a penetration tester',
-        'date_posted': 'July 20, 2021'
-    }
-]
+
 
 # Create your views here.
 def index(request):
     
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
